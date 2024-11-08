7d6b08d49a83e3bd3969f170d0f6a1aa23865816
Alex Varghese <alexvp200325@gmail.com>
2024-11-08 11:06:02 +0530

    Render home

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const env = require('dotenv').config();
 const db = require('./config/db');
+const userRouter = require('./routes/userRouter')
 db();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on : http://localhost:${process.env.PORT}`);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+app.set('views', [
+    path.join(__dirname,'/views/user'), 
+    path.join(__dirname,'/views/admin')
+]);
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/', userRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on : http://localhost:${PORT}`);
 });
 
 
