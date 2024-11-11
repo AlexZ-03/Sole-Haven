4b88b115aeee64bc6d6a5eb880a6b04a59d339a8
Alex Varghese <alexvp200325@gmail.com>
2024-11-11 20:21:51 +0530

    Customer Mangagement

---
+++ controllers/admin/customerController.js
@@ -0,0 +1,73 @@
+const User = require('../../models/userSchema');
+const mongoose = require('mongoose');
+
+
+const customerInfo = async (req, res) => {
+    try {
+        let search = "";
+        if(req.query.search){
+            search = req.query.search;
+        }
+        let page = 1;
+        if(req.query.page){
+            page = req.query.page;
+        }
+        const limit = 3;
+        const userData = await User.find({
+            isAdmin: false,
+            $or: [
+                {name: {$regex: ".*"+search+".*"}},
+                {email: {$regex: ".*"+search+".*"}}
+            ],
+        })
+        .limit(limit*1)
+        .skip((page-1)*limit)
+        .exec();
+
+        const count = await User.find({
+            isAdmin: false,
+            $or: [
+                {name: {$regex: ".*"+search+".*"}},
+                {email: {$regex: ".*"+search+".*"}}
+            ],
+        })
+        .countDocuments();
+
+        res.render('customers', { data: userData, page, totalPages: Math.ceil(count / limit) });
+    } catch (error) {
+        
+    }
+}
+
+const customerBlock = async (req, res) => {
+    try {
+        let id = req.query.id;
+        await User.updateOne({_id: id}, {$set: {isBlocked: true}});
+        const user = await User.findOne({ _id: id }).select('name');
+        console.log(`user ${user.name} is Blocked`);
+        res.redirect("/admin/users");
+    } catch (error) {
+        console.log('Error as occurred during customerBlock', error);
+        res.status(500).send("Internal Server Error");
+    }
+}
+
+const customerUnblock = async (req, res) => {
+    try {
+        let id = req.query.id;
+        await User.updateOne({_id: id}, {$set: {isBlocked: false}});
+        const user = await User.findOne({ _id: id }).select('name');
+        console.log(`user ${user.name} is Unblocked`);
+        res.redirect('/admin/users');
+    } catch (error) {
+        console.log("Error as occured during customerUnblock");
+        res.status(500).send("Internal Server Error");
+        }
+}
+
+
+module.exports = {
+    customerInfo,
+    customerBlock,
+    customerUnblock,
+}
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin/adminController");
+const customerController = require("../controllers/admin/customerController");
 const {userAuth, adminAuth} = require('../middlewares/auth');
 
 router.get('/login', adminController.loadLogin);
@@ -8,8 +9,9 @@ router.post('/login', adminController.login);
 router.get('/', adminAuth, adminController.loadDashboard);
 router.get('/logout', adminController.logout);
 
-
-
+router.get('/users', adminAuth, customerController.customerInfo);
+router.get('/blockCustomer', adminAuth, customerController.customerBlock);
+router.get('/unblockCustomer', adminAuth, customerController.customerUnblock);
 
 
 
